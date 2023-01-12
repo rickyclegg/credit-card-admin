31bf10f8f0d7558c0a1d1f4e192a6d10b697212c
Ricky Clegg <rick@22px.io>
2023-01-12 13:05:56 +0000

    feat: add e2e test for adding cards. Currently failing

--- black-box-tests/acceptance-tests/cypress/e2e/spec.cy.js
+++ black-box-tests/acceptance-tests/cypress/e2e/spec.cy.js
@@ -1,9 +1,31 @@
 describe('template spec', () => {
   const getNoCardsWarning = () => cy.get('[data-testid="no-cards-warning"]')
+  const getCardRow = (value) => cy.get('#cardsTable tr').eq(value)
 
   it('has no credit cards when starting', () => {
     cy.visit('/')
 
     getNoCardsWarning().should('be.visible');
+  })
+
+  it('adds a new card to the system', () => {
+    const expectedName = 'Alice'
+    const expectedCardNum = '1111 2222 3333 4444'
+    const expectedLimit = 2000
+    const expectedBalance = 0
+
+    cy.visit('/')
+
+    cy.get('#name').type(expectedName)
+    cy.get('#cardNumber').type(expectedCardNum)
+    cy.get('#limit').type(expectedLimit)
+    cy.get('input[type="submit"]').click()
+
+    const newCardRow = getCardRow(1)
+
+    newCardRow.get('.card-name').contains(expectedName)
+    newCardRow.get('.card-number').contains(expectedCardNum)
+    newCardRow.get('.card-limit').contains(expectedLimit)
+    newCardRow.get('.card-balance').contains(expectedBalance)
   })
 })
